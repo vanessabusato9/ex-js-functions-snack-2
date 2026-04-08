65a734850cc531801264b1f5214a680631e29d29
vanessa <vane.busato9@gmail.com>
2026-04-08 21:16:19 +0200

    Snack 1

---
+++ index.js
@@ -0,0 +1,15 @@
+//SNACK 1
+function somma(Num1, Num2) {
+    return Num1 + Num2;
+}
+
+const somma = function (Num1, Num2) {
+    return Num1 + Num2;
+}
+
+const somma = (Num1, Num2) => Num1 + Num2;
+
+
+
+
+
